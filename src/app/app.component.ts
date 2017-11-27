79d05c2c857131bf0de0e430c6e0110f7155949d
Mark  Healy <markhealy@hiot.io>
2017-11-27 12:19:00 +0000

    tidy up main view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-import {MaterialModule} from './material/material.module';
+import { MaterialModule } from './material/material.module';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  menuItems = [
+    {
+      url: '/dashboard',
+      icon: 'dashboard',
+      text: 'Dashboard'
+    },
+    {
+      url: '/control',
+      icon: 'play_arrow',
+      text: 'Control'
+    },
+    {
+      url: '/data',
+      icon: 'donut_small',
+      text: 'Status'
+    },
+    {
+      url: '/settings',
+      icon: 'build',
+      text: 'Settings'
+    }
+  ];
   title = 'HIOT Commander';
+  go(url) {
+
+  }
 }
